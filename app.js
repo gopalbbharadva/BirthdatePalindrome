4ecbd83c9f7e7727e4366bbef7ab649272b13917
gopalbbharadva <gopalbbharadva@gmail.com>
2021-08-05 19:49:38 +0530

    fix:days and date false bug

--- app.js
+++ app.js
@@ -2,45 +2,56 @@ var date = document.querySelector("#date");
 var checkButton = document.querySelector("#checkButton");
 var output = document.querySelector(".result");
 var form = document.querySelector("form");
-
-var result1 = `<h1>Not palindrome</h1>`;
-var result2 = `<h1>Palindrome</h1>`;
+var gif = document.querySelector("#timerGif");
+var error = document.querySelector("#error");
 const lastDateArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-form.addEventListener("submit", (e) => {
+checkButton.addEventListener("click", (e) => {
   e.preventDefault();
-  setTimeout(() => {
-    start();
-  }, 1000);
+  error.style.display = "none";
+  if (date.value != "") {
+    gif.style.display = "block";
+    setTimeout(() => {
+      start();
+      gif.style.display = "none";
+      output.style.display = "block";
+    }, 3000);
+  } else {
+    error.style.display = "block";
+  }
 });
 
 function start() {
   var dateArray = date.value.split("-");
+  console.log(dateArray);
   var year = dateArray[0];
   var month = dateArray[1];
   var day = dateArray[2];
-  var setResult = isPalindrome(year, month, day);
+  var setResult = checkCombination(year, month, day);
   if (setResult)
     output.innerHTML = `<h2 style="color:#3B82F6; font-weight:bold;">Nice!! your birthdate is palindrome with seqence of 
   <span style="color:black">${setResult}</span></h2>`;
   else {
     let [palindromeDate, days] = farDate(day, month, year);
-    output.innerHTML = `<p id="text">Sorry you were left with <span style="color:#EF4444;font-weight:bold"> ${days} </span>
-    days and nearest birthdate is <span style="color:#3B82F6;font-weight:bold"> ${palindromeDate} </span> </p>`;
+    output.innerHTML = `<p style="margin:0" >Sorry you were left with <span style="color:#EF4444;font-weight:bold"> ${days} </span>
+    days and nearest birthdate is <span style="color:#3B82F6;font-weight:bold">
+     ${palindromeDate} </span> </p>`;
   }
 }
 
-function isPalindrome(yyyy, mm, dd) {
+function checkCombination(yyyy, mm, dd) {
   var df1 = yyyy + mm + dd;
-  var df2 = mm + dd + yyyy;
+  var df2 = dd + mm + yyyy;
   var df3 = mm + dd + yyyy.substring(2);
-  if (checkPalindrome(df1)) return `${yyyy}-${mm}-${dd}`;
-  else if (checkPalindrome(df2)) return `${mm}-${dd}-${yyyy}`;
-  else if (checkPalindrome(df3)) return `${mm}-${dd}-${yyyy.substring(2)}`;
+  var df4 = Number(mm) + dd + yyyy;
+  if (isPalindrome(df1)) return `${yyyy}-${mm}-${dd}`;
+  else if (isPalindrome(df2)) return `${dd}-${mm}-${yyyy}`;
+  else if (isPalindrome(df3)) return `${mm}-${dd}-${yyyy.substring(2)}`;
+  else if (isPalindrome(df4)) return `${Number(mm)}-${dd}-${yyyy}`;
   else return null;
 }
 
-function checkPalindrome(dateString) {
+function isPalindrome(dateString) {
   for (var i = 0; i < Math.floor(dateString.length / 2); i++) {
     if (dateString[i] != dateString[dateString.length - 1 - i]) return false;
   }
@@ -60,7 +71,7 @@ function farDate(day, month, year) {
     if (df > Number(lastDateArray[mf - 1])) {
       df = 1;
       mf += 1;
-      if (yf > 12) {
+      if (mf > 12) {
         mf = 1;
         yf += 1;
       }
@@ -68,9 +79,10 @@ function farDate(day, month, year) {
     var dString = df.toString();
     var mString = mf.toString();
     var yString = yf.toString();
-    if (dString.length === 1) dString = `0+${dString}`;
-    if (mString.lenght === 1) mString = `0+${mString}`;
-    var returnedDate = isPalindrome(yString, mString, dString);
+    if (dString.length == 1) dString = `0${dString}`;
+    if (mString.length == 1) mString = `0 ${mString}`;
+    var returnedDate = checkCombination(yString, mString, dString);
+    console.log(returnedDate);
     if (returnedDate) return [`${returnedDate}`, j];
 
     if (yb > 1) {
@@ -88,9 +100,9 @@ function farDate(day, month, year) {
     var dString = db.toString();
     var mString = mb.toString();
     var yString = yb.toString();
-    if (dString.lenght === 1) dString = `0` + dString;
-    if (mString.lenght === 1) mString = `0` + mString;
-    var returnedDate = isPalindrome(yString, mString, dString);
+    if (dString.length == 1) dString = `0${dString}`;
+    if (mString.length == 1) mString = `0${mString}`;
+    var returnedDate = checkCombination(yString, mString, dString);
     if (returnedDate) return [`${returnedDate}`, j];
   }
 }
